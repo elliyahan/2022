9053738a172331653d63725da891ff2c4786af48
JeongA Han <mongndam@naver.com>
2022-07-26 21:01:20 +0900

    그루공방 react 폴더 생성

--- mypage/src/component/StoryBoard/StoryBoard4.jsx
+++ mypage/src/component/StoryBoard/StoryBoard4.jsx
@@ -4,7 +4,13 @@ const StoryBoard4 = () => {
   return (
     <div id='StoryBoard4'>
       <div className='storyboard-img-wrap'>
-        <img src="/img/storyboard/storyboard/storyboard3_1.jpg" alt="" />
+        <img src="/img/storyboard/storyboard/storyboard4_1.jpg" alt="" />
+      </div>
+      <div className='storyboard-img-wrap'>
+        <img src="/img/storyboard/storyboard/storyboard4_2.jpg" alt="" />
+      </div>
+      <div className='storyboard-img-wrap'>
+        <img src="/img/storyboard/storyboard/storyboard4_3.jpg" alt="" />
       </div>
     </div>
   );
